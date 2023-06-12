474d6f65e1f069bcebb81caef594b956694d3b32
zhoudadudu <72678330+zhoudadudu@users.noreply.github.com>
2023-06-12 18:39:21 +0800

    Add files via upload

---
+++ pokemon/data/attack.js
@@ -0,0 +1,15 @@
+const attacks = {
+    Tackle: {
+        name: 'Tackle',
+        damage: 10,
+        type: 'Normal',
+        color: 'black'
+    },
+    Fireball: {
+        name: 'Fireball',
+        damage: 100,
+        type: 'Fire',
+        color: 'red'
+
+    }
+}
